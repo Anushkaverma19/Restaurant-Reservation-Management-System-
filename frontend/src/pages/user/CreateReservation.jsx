80f78e9284a9a034881438db4effe502f99129bd
unknown <10anushka2023@gmail.com>
2026-07-19 21:25:10 +0530

    fix ui

--- frontend/src/pages/user/CreateReservation.jsx
+++ frontend/src/pages/user/CreateReservation.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api/axios";
+
 const CreateReservation = () => {
   const navigate = useNavigate();
 
@@ -10,13 +11,12 @@ const CreateReservation = () => {
     reservationDate: "",
     startTime: "",
     endTime: "",
-    guests: "",
+    guests: 1,
   });
 
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,6 +29,7 @@ const CreateReservation = () => {
 
 
   const validateForm = () => {
+
     if (!formData.reservationDate) {
       return "Please select reservation date";
     }
@@ -62,6 +63,7 @@ const CreateReservation = () => {
 
     const token = localStorage.getItem("token");
 
+
     if (!token) {
       navigate("/login");
       return;
@@ -69,30 +71,34 @@ const CreateReservation = () => {
 
 
     try {
+
       setLoading(true);
       setMessage("");
 
- await API.post(
-  "/reservations",
-  formData,
-  {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }
-);
+
+      await API.post(
+        "/reservations",
+        formData,
+        {
+          headers:{
+            Authorization:`Bearer ${token}`,
+          },
+        }
+      );
+
 
       setMessage(
         "Reservation created successfully"
       );
 
 
-      setTimeout(() => {
+      setTimeout(()=>{
         navigate("/my-reservations");
-      }, 1200);
+      },1200);
 
 
-    } catch (error) {
+
+    } catch(error){
 
       console.error(
         "Reservation Error:",
@@ -105,14 +111,20 @@ const CreateReservation = () => {
         "Unable to create reservation"
       );
 
+
     } finally {
+
       setLoading(false);
+
     }
+
   };
 
 
   return (
+
     <div className="min-h-screen bg-slate-950 flex items-center justify-center px-4">
+
 
       <form
         onSubmit={handleSubmit}
@@ -126,45 +138,56 @@ const CreateReservation = () => {
         "
       >
 
-        <h1 className="
-        text-3xl 
-        font-bold 
-        text-center 
+
+        <h1
+        className="
+        text-3xl
+        font-bold
+        text-center
         mb-6
-        ">
+        "
+        >
           Create Reservation
         </h1>
 
+
+
+        {/* Date */}
 
         <div className="mb-4">
 
           <label className="block mb-2">
             Reservation Date
           </label>
-<input
-  type="date"
-  name="reservationDate"
-  min={today}
-  value={formData.reservationDate}
-  onChange={handleChange}
-  required
-  className="
-  w-full
-  p-3
-  rounded-lg
-  bg-slate-800
-  border
-  border-slate-600
-  text-white
-  focus:outline-none
-  focus:ring-2
-  focus:ring-yellow-500
-  "
-/>
+
+
+          <input
+            type="date"
+            name="reservationDate"
+            min={today}
+            value={formData.reservationDate}
+            onChange={handleChange}
+            required
+            className="
+            w-full
+            p-3
+            rounded-lg
+            bg-slate-800
+            border
+            border-slate-600
+            text-white
+            focus:outline-none
+            focus:ring-2
+            focus:ring-yellow-500
+            "
+          />
 
         </div>
 
 
+
+
+        {/* Start Time */}
 
         <div className="mb-4">
 
@@ -172,30 +195,33 @@ const CreateReservation = () => {
             Start Time
           </label>
 
-        <input
-  type="time"
-  name="reservationDate"
-  min={today}
-  value={formData.reservationDate}
-  onChange={handleChange}
-  required
-  className="
-  w-full
-  p-3
-  rounded-lg
-  bg-slate-800
-  border
-  border-slate-600
-  text-white
-  focus:outline-none
-  focus:ring-2
-  focus:ring-yellow-500
-  "
-/>
+
+          <input
+            type="time"
+            name="startTime"
+            value={formData.startTime}
+            onChange={handleChange}
+            required
+            className="
+            w-full
+            p-3
+            rounded-lg
+            bg-slate-800
+            border
+            border-slate-600
+            text-white
+            focus:outline-none
+            focus:ring-2
+            focus:ring-yellow-500
+            "
+          />
 
         </div>
 
 
+
+
+        {/* End Time */}
 
         <div className="mb-4">
 
@@ -203,30 +229,33 @@ const CreateReservation = () => {
             End Time
           </label>
 
-        <input
-  type="time"
-  name="reservationDate"
-  min={today}
-  value={formData.reservationDate}
-  onChange={handleChange}
-  required
-  className="
-  w-full
-  p-3
-  rounded-lg
-  bg-slate-800
-  border
-  border-slate-600
-  text-white
-  focus:outline-none
-  focus:ring-2
-  focus:ring-yellow-500
-  "
-/>
+
+          <input
+            type="time"
+            name="endTime"
+            value={formData.endTime}
+            onChange={handleChange}
+            required
+            className="
+            w-full
+            p-3
+            rounded-lg
+            bg-slate-800
+            border
+            border-slate-600
+            text-white
+            focus:outline-none
+            focus:ring-2
+            focus:ring-yellow-500
+            "
+          />
 
         </div>
 
 
+
+
+        {/* Guests */}
 
         <div className="mb-4">
 
@@ -235,28 +264,29 @@ const CreateReservation = () => {
           </label>
 
 
-       <input
-  type="number"
-  name="reservationDate"
-  min={today}
-  value={formData.reservationDate}
-  onChange={handleChange}
-  required
-  className="
-  w-full
-  p-3
-  rounded-lg
-  bg-slate-800
-  border
-  border-slate-600
-  text-white
-  focus:outline-none
-  focus:ring-2
-  focus:ring-yellow-500
-  "
-/>
+          <input
+            type="number"
+            name="guests"
+            min="1"
+            value={formData.guests}
+            onChange={handleChange}
+            required
+            className="
+            w-full
+            p-3
+            rounded-lg
+            bg-slate-800
+            border
+            border-slate-600
+            text-white
+            focus:outline-none
+            focus:ring-2
+            focus:ring-yellow-500
+            "
+          />
 
         </div>
+
 
 
 
@@ -286,21 +316,24 @@ const CreateReservation = () => {
 
 
 
+
         {
           message && (
 
             <p
-              className={`
-              text-center
-              mt-4
-              ${
-                message.includes("success")
-                ? "text-green-400"
-                : "text-red-400"
-              }
-              `}
+            className={`
+            text-center
+            mt-4
+            ${
+              message.includes("success")
+              ? "text-green-400"
+              : "text-red-400"
+            }
+            `}
             >
+
               {message}
+
             </p>
 
           )
@@ -309,7 +342,9 @@ const CreateReservation = () => {
 
       </form>
 
+
     </div>
+
   );
 };
 
